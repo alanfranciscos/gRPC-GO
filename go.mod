5a20602e2edf5dfc4fec3d1e1172c16dc2fc79c0
Alan Silva <alanfranciscos@gmail.com>
2024-01-25 18:13:27 -0300

    feat: init project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/alanfranciscos/gRPC-GO
+
+go 1.18
+
+require github.com/google/uuid v1.6.0
---
+++ internal/databases/course.go
@@ -0,0 +1,88 @@
+package database
+
+import (
+	"database/sql"
+	"github.com/google/uuid"
+)
+
+type Course struct{
+	db *sql.DB
+	Id string
+	Name string
+	Description string
+	CategoryId string
+}
+
+func NewCourse(db *sql.DB) *Course{
+	return &Course{db:db}
+}
+
+func (c *Course) Create(name, description, categoryId string) (*Course, error){
+	id:= uuid.New().String()
+	query := "INSERT INTO courses (id, name, description, category_id) VALUES ($1, $2, $3, $4)"
+	_, err := c.db.Exec(query, id, name, description, categoryId)
+
+	if err != nil{
+		return nil, err
+	}
+
+	return &Course{
+		Id: id,
+		Name: name,
+		Description: description,
+		CategoryId: categoryId,
+	},nil
+}
+
+func (c *Course) FindAll() ([]Course, error){
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses")
+	if err != nil{
+		return nil, err
+	}
+
+	defer rows.Close()
+	courses := []Course{}
+
+	for rows.Next(){
+		var id, name, description,categoryId string
+		if err := rows.Scan(&id, &name, &description, &categoryId); err != nil{
+			return nil, err
+		}
+
+		courses = append(courses, Course{
+			Id: id,
+			Name: name,
+			Description: description,
+			CategoryId: categoryId,
+		})
+	}
+
+	return courses, nil
+}
+
+func (c *Course) FindByCategoryID(categoryID string) ([]Course, error){
+	rows, err := c.db.Query("SELECT id, name, description, category_id FROM courses WHERE category_id = $1", categoryID)
+
+	if err != nil{
+		return nil, err
+	}
+
+	defer rows.Close()
+	courses := []Course{}
+
+	for rows.Next(){
+		var id, name, description, categoryId string
+		if err := rows.Scan(&id, &name, &description, &categoryId); err != nil{
+			return nil, err
+		}
+
+		courses = append(courses, Course{
+			Id: id,
+			Name: name,
+			Description: description,
+			CategoryId: categoryId,
+		})
+	}
+
+	return courses, nil
+}
